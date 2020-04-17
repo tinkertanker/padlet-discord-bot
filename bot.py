6ff5a0af8fbe06c504908591ef259da0cb87f4a5
Navneeth <knav@users.noreply.github.com>
2020-04-17 13:49:15 +0800

    final commit, last fixes and cleaning up of config files

--- bot.py
+++ bot.py
@@ -11,21 +11,18 @@ import os
 
 
 load_dotenv()
+token = os.getenv('TOKEN')
+padlet_appid = os.getenv('APPID')
 
 
-with open("config_test.json") as json_file:
+with open("config_main.json") as json_file:
     vals = json.load(json_file)
 padlet_appid = vals['Padlet']['App ID']
 padlet_url = vals['Padlet']['URL']
 t_delay = vals['Discord']['Time between loop']  # in minutes
-try:
-    token = os.getenv('TOKEN')
-    print("grabbed from .env file")
-except:
-    token = vals['Discord']['Bot Client ID']
-    print("dunno what ur talking about man")
 bot_call = vals['Discord']['Bot Mention']
 valid_channels = vals['Discord']['Channels']['allowed']
+posting_channel = vals['Discord']['Channels']['posting-channel']
 
 
 client = commands.Bot(command_prefix=bot_call)
@@ -66,7 +63,7 @@ async def embed_generator(channel, in_list):
 
 async def background_timer():
     await client.wait_until_ready()
-    channel = client.get_channel(697682410581786714)
+    channel = client.get_channel(posting_channel)
     while not client.is_closed():
         start = time.time()
         response = requests.get(
@@ -79,11 +76,11 @@ async def background_timer():
                 relevant_data = []
                 for post in extract_data:
                     post['updated_at'] = dt.datetime.strptime(post['updated_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                    if post['updated_at'] > (dt.datetime.utcnow() - dt.timedelta(minutes=45)):
+                    if post['updated_at'] > (dt.datetime.utcnow() - dt.timedelta(minutes=t_delay)):
                         relevant_data.append(post)
                 if len(relevant_data) > 5:
                     num_posts = len(relevant_data)
-                    contrib_list = [get_author_name(x['author_id']) for x in relevant_data]
+                    contrib_list = list(set([get_author_name(x['author_id']) for x in relevant_data]))
                     if len(contrib_list) > 3:
                         contrib_str = ', '.join(contrib_list[0:2])
                         contrib_str += ", and more."
@@ -97,7 +94,7 @@ async def background_timer():
                     await channel.send(content=f"{num_posts} new posts have been made by {contrib_str}")
                     trunc_data = relevant_data[:3]
                     await embed_generator(channel, trunc_data)
-                    await channel.send(content="... and more \n See all posts at https://padlet.tk.sg/swift2020submissions")
+                    await channel.send(content="... and more! \n All posts are on https://padlet.tk.sg/swift2020submissions")
                 elif len(relevant_data) > 0:
                     await embed_generator(channel, relevant_data)
             else:
@@ -107,7 +104,7 @@ async def background_timer():
             print("There has been an error in processing this request.")
             print(response.status_code)
         n_delay = (t_delay * 60) - (time.time() - start)
-        await asyncio.sleep(30)  # asyncio.sleep takes delay in seconds
+        await asyncio.sleep(n_delay)  # asyncio.sleep takes delay in seconds
 
 
 async def submissions_counter():
@@ -135,7 +132,7 @@ async def hello(ctx):
     if str(ctx.channel) in valid_channels:
         await ctx.send("Hi!")
     else:
-        await ctx.send("I am not welcome here.")
+        await ctx.send("I am not welcome here :(")
 
 
 @client.command(aliases=['submissions'], name="submission",
